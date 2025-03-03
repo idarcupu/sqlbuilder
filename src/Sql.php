b37d5cc3131a97bdf22573c969e9282797f20ac0
idarlabs <idarlabs@gmail.com>
2025-03-03 11:16:25 +0700

    Sen 03 Mar 2025 11:16:25  WIB

--- src/Sql.php
+++ src/Sql.php
@@ -7,62 +7,63 @@ use Illuminate\Support\Str;
 
 class Sql {
 
-  protected $query;
-  protected $table;
-  protected $alias;
+    protected $query;
+    protected $table;
+    protected $alias;
 
-  public function __construct(){
-    $this->table = null;
-    $this->alias = null;
-    $this->query = null;
-  }
+    public function __construct(){
+      $this->table = null;
+      $this->alias = null;
+      $this->query = null;
+    }
 
-  public static function __callStatic($name, $arguments){
-    $instance = new self();
-    $instance->table = Str::snake($name);
-    $instance->query = DB::table($instance->table);
-    return $instance;
-  }
+    public static function __callStatic($name, $arguments){
+      $instance = new self();
+      $instance->table = Str::snake($name);
+      $instance->query = DB::table($instance->table);
+      return $instance;
+    }
 
-  public function alias($alias){
-    $this->alias = $alias;
-    $this->query = DB::table("{$this->table} as {$this->alias}");
-    return $this;
-  }
+    public function alias($alias){
+      $this->alias = $alias;
+      $this->query = DB::table("{$this->table} as {$this->alias}");
+      return $this;
+    }
 
-  public function all($conditions = []){
-    if(!empty($conditions) && is_array($conditions)){
-      foreach ($conditions as $column => $value){
-        if($this->alias){
-          $column = "{$this->alias}.{$column}";
+    public function all($conditions = []){
+      $query = clone $this->query;
+
+      if(!empty($conditions) && is_array($conditions)){
+        foreach ($conditions as $column => $value){
+          if($this->alias && !str_contains($column, '.')){
+            $column = "{$this->alias}.{$column}";
+          }
+          $query->where($column, $value);
         }
-        $this->query->where($column, $value);
       }
+      return $query->get();
     }
-    return $this->query->get();
-  }
 
-  public function one($conditions = []){
-    if(!empty($conditions) && is_array($conditions)){
-      foreach ($conditions as $column => $value){
-        if($this->alias){
-          $column = "{$this->alias}.{$column}";
+    public function one($conditions = []){
+      $query = clone $this->query;
+
+      if(!empty($conditions) && is_array($conditions)){
+        foreach ($conditions as $column => $value){
+          if($this->alias && !str_contains($column, '.')){
+            $column = "{$this->alias}.{$column}";
+          }
+          $query->where($column, $value);
         }
-        $this->query->where($column, $value);
       }
-    }
-    return $this->query->first();
-  }
-
-  public function __call($method, $arguments){
-    if(in_array($method, ['all', 'one'])){
-      return call_user_func_array([$this, $method], $arguments);
+      return $query->first();
     }
 
-    if(method_exists($this->query, $method)){
-      return call_user_func_array([$this->query, $method], $arguments);
-    }
+    public function __call($method, $arguments){
+      if(method_exists($this->query, $method)){
+        $this->query = call_user_func_array([$this->query, $method], $arguments);
+        return $this;
+      }
 
-    throw new \BadMethodCallException("Method {$method} does not exist.");
-  }
+      throw new \BadMethodCallException("Method {$method} does not exist.");
+    }
 }
